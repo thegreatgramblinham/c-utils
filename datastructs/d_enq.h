98504e779a17093e6908ad77007a520d10330bd2
Sam Hudock <shudock@altair.com>
2023-09-12 14:14:09 -0400

    Adds initial commit with header file outlines

---
+++ datastructs/d_enq.h
@@ -0,0 +1,15 @@
+
+// D_ENQ.h
+// This module defines a *D*ouble directional, *ENQ*ueue-able structure
+// (and related types) that can be declared as the first member of another
+// structure to hold arbitrary data in a doubly linked list format.
+
+#ifndef D_ENQ_H
+#define D_ENQ_H
+
+///Imports
+#include "s_enq.h"
+
+///Constants
+///Extern Declarations
+#endif
---
+++ datastructs/s_enq.h
@@ -0,0 +1,13 @@
+
+// S_ENQ.h
+// This module defines a *S*ingle directional, *ENQ*ueue-able structure
+// (and related types) that can be declared as the first member of another
+// structure to hold arbitrary data in a singly linked list format.
+
+#ifndef S_ENQ_H
+#define S_ENQ_H
+
+///Imports
+///Constants
+///Extern Declarations
+#endif
