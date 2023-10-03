a8f6bf8e967f47ba9934afdfaaeeea054c81ae70
Sam Hudock <shudock@altair.com>
2023-10-03 13:51:39 -0400

    Continues outlining singly linked list

---
+++ datastructs/s_enq.c
@@ -0,0 +1,11 @@
+
+///Imports
+#include "s_enq.h"
+#include <stdlib.h>
+#include <bool.h>
+
+///Static Constants
+///Static Global Variables
+///Local Declarations
+///Static Functions
+///Extern Functions
